53f490b0b9fa3a336de9de1bb1a33c293e56380c
Arturo Puente <arturopuentevc@gmail.com>
2015-11-27 10:09:06 -0500

    feat(appstore): First commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
  scope path: '/api' do
    api_version(:module => "Api::V1", :path => {:value => "v1"}) do
    end
